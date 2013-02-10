0e8de573da5c126ee945c4dd1587b770473ea2c9
Jim Kang <jimkang@gmail.com>
2013-02-10 18:57:50 -0500

    Item dragging is now working, although the scores were temporarily dropped.
    Stopped using the Item template for the item rect contents because that stopped the draggability, and Meteor reactivity was cut off anyway.
    
    Added drag behaviors – one for plain rects and one for groups (<g>s). groupdragmove moves its children because the <g> does not have its own position.
    
    Dropped css for elements inside <svg> because that prevents dragging somehow.

--- boxadder.js
+++ boxadder.js
@@ -11,6 +11,20 @@ if (Meteor.isClient) {
 		return group;
 	};
 	
+	// Drag behaviors that can be added to SVG selections.
+	function dragmove(d) {
+	  d3.select(this)
+      .attr("x", d.x = d3.event.x)
+      .attr("y", d.y = d3.event.y);
+	}
+	var drag = d3.behavior.drag().origin(Object).on("drag", dragmove);	
+
+	function groupdragmove(d) {
+	  d3.selectAll(this.childNodes)
+      .attr("x", d.x = d3.event.x)
+      .attr("y", d.y = d3.event.y);
+	}
+	var groupdrag = d3.behavior.drag().origin(Object).on("drag", groupdragmove);	
 	
 	Template.board.rendered = function () {
 	  var self = this;
@@ -20,7 +34,7 @@ if (Meteor.isClient) {
 	    self.handle = Meteor.autorun(function () {					
 	      // Draw a rect for each box or item object
 	      var updateRectObjects = function (group, cssClass) {
-					setD3GroupAttrsWithProplist(group, 
+					return setD3GroupAttrsWithProplist(group, 
 						["_id", "x", "y", "width", "height"])
 					// Need to set class for CSS. Setting attr seems to clear everything 
 					// that's not explicitly set.
@@ -31,19 +45,21 @@ if (Meteor.isClient) {
 					setD3GroupAttrsWithProplist(group, 
 						["_id", "x", "y", "width", "height"])
 						.classed("itemLabel", true);
-	      };
+	      };				
 				
 	      var boxesDrawings = 
 					d3.select(self.node).select(".boxZone").selectAll("rect .box")
 	        	.data(Boxes.find().fetch(), function (box) { return box._id; });
 	
-	      updateRectObjects(boxesDrawings.enter().append("rect"), "box");
+				var boxesSelection = boxesDrawings.enter().append("rect").call(drag);
+	      updateRectObjects(boxesSelection, "box")
+				.attr("fill", function(d) { return "red"; });
 				
 				// Create <foreignObject> elements for each box to hold sums.
 	      var sumLabels = 
 					d3.select(self.node).select(".sumLabels").selectAll("foreignObject")
 						.data(Boxes.find().fetch(), function (box) { return box._id; });
-					
+									
 				sumLabels.enter().append("foreignObject").classed("sumLabel", true)
 					.attr("_id", function (box) { return box._id; })
 					.attr("x", function (box) { return box.x + box.width - 100; })
@@ -52,41 +68,54 @@ if (Meteor.isClient) {
 					.attr("height", function (box) { return 44; });
 				// Populate those foreign objects with the sum template.
 				$('.sumLabel').each(function (index, foreignObject) {
+					// Be careful to give the search criteria to find(), not to fetch().
 					var theBox = 
 						Boxes.find({ _id: $(foreignObject).attr("_id") }).fetch()[0];
-
+				
 					$(foreignObject).append(
 						"<body xmlns=\"http://www.w3.org/1999/xhtml\"></body>")
-					// Be careful to specify the search critera to find(), not fetch().
-					.append(Template.sumContainer(theBox));
-					});
-				
+					.append(Template.sumContainer(theBox));					
+				});
+								
 				sumLabels.exit().remove();
-
-	      var itemDrawings = 
-					d3.select(self.node).select(".boxZone").selectAll("rect .item")
-	        	.data(Items.find().fetch(), function (item) { return item._id; });
-	
-	      updateRectObjects(itemDrawings.enter().append("rect"), "item");
 				
+	      var itemDrawings = 
+					d3.select(self.node).select(".boxZone").selectAll("g .item")
+	        	.data(Items.find().fetch(), function (item) { return item._id; });
+					
+				var itemGroupSelection = itemDrawings.enter().append("g").call(groupdrag);
+					
+				// Set up the text.
+				setD3GroupAttrsWithProplist(
+					itemGroupSelection.append("text").text(function (d) { return d.title; }), 
+					["_id", "x", "y", "width", "height"]);
+				
+	      updateRectObjects(itemGroupSelection
+					.append("rect").attr("fill", function(d) { return "blue"; }), 
+					"item");
+					
+								
 				// Create <foreignObject> elements for each item.
 	      var itemLabels = 
 					d3.select(self.node).select(".itemLabels").selectAll("foreignObject")
 					.data(Items.find().fetch(), function (item) { return item._id; });
 				// console.log(labels);
-				
-				positionItemLabels(itemLabels.enter().append("foreignObject"));
+								
+				// positionItemLabels(itemLabels.enter().append("text")
+				// 	.text(function (d) { return d.title; }));
+					
 				// Populate the foreignObject items using the item template.
-				$('.itemLabel').each(function (index, foreignObject) { 
-					$(foreignObject).append(
-						"<body xmlns=\"http://www.w3.org/1999/xhtml\"></body>")
-					// Be careful to specify the search critera to find(), not fetch().
-					.append(
-						Template.item(Items.find({ _id: $(foreignObject).attr("_id") })
-						.fetch()[0]));
-					});
-				
+				// $('.itemLabel').each(function (index, foreignObject) { 
+				// 	$(foreignObject).append(
+				// 		"<body xmlns=\"http://www.w3.org/1999/xhtml\"></body>")
+				// 	// Be careful to specify the search critera to find(), not fetch().
+				// 	.append(
+				// 		Template.item(Items.find({ _id: $(foreignObject).attr("_id") })
+				// 		.fetch()[0]));
+				// 	});
+				// 				
 				itemLabels.exit().remove();
+								
 			});
 		}	
 	};
