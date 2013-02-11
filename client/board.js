472d6e7edfe468fc9df7f5c440cbabfb5a62a50c
Jim Kang <jimkang@gmail.com>
2013-02-11 00:49:33 -0500

    More refactoring: Broke setUpSums and setUpItems out of board.rendered.

--- client/board.js
+++ client/board.js
@@ -38,7 +38,70 @@ function positionItemLabels(group) {
 	setD3GroupAttrsWithProplist(group, 
 		["_id", "x", "y", "width", "height"])
 		.classed("itemLabel", true);
-};				
+};
+
+/* Element set up/syncing. */
+
+function setUpBoxes(svgNode) {
+  // Append Boxes to the existing box rects.
+  var boxesSelection = 
+		d3.select(svgNode).select(".boxZone").selectAll("rect .box")
+    	.data(Boxes.find().fetch(), function (box) { return box._id; });
+
+	// Sync the Boxes data with the box rects. Add drag behavior to them.
+	boxesSelection.enter().append("rect").call(drag);
+  syncCommonRectAttrs(boxesSelection, "box")
+	.attr("fill", function(d) { return "red"; });
+	
+	boxesSelection.exit().remove();
+}
+
+function setUpSums(svgNode) {
+	// Create <foreignObject> elements for each box to hold sums.
+  var sumLabels = 
+		d3.select(svgNode).select(".sumLabels").selectAll("foreignObject")
+			.data(Boxes.find().fetch(), function (box) { return box._id; });
+			
+	sumLabels.enter().append("foreignObject").classed("sumLabel", true)
+		.attr("_id", function (box) { return box._id; })
+		.attr("x", function (box) { return box.x + box.width - 100; })
+		.attr("y", function (box) { return box.y + box.height - 44; })
+		.attr("width", function (box) { return 100; })
+		.attr("height", function (box) { return 44; });
+				
+	// Populate those foreign objects with the sum template.
+	$('.sumLabel').each(function (index, foreignObject) {
+		// Be careful to give the search criteria to find(), not to fetch().
+		var theBox = 
+			Boxes.find({ _id: $(foreignObject).attr("_id") }).fetch()[0];
+				
+		$(foreignObject).append(
+			"<body xmlns=\"http://www.w3.org/1999/xhtml\"></body>")
+		.append(Template.sumContainer(theBox));					
+	});	
+	
+	sumLabels.exit().remove();	
+}
+
+function setUpItems(svgNode) {
+	// Sync the <g>s and the data.
+  var itemGroupSelection = 
+		d3.select(svgNode).select(".boxZone").selectAll("g .item")
+    	.data(Items.find().fetch(), function (item) { return item._id; });
+					
+	itemGroupSelection.enter().append("g").call(groupdrag);
+					
+	// Set up the text.
+	setD3GroupAttrsWithProplist(
+		itemGroupSelection.append("text").text(function (d) { return d.title; }), 
+		["_id", "x", "y", "width", "height"]);
+				
+  syncCommonRectAttrs(itemGroupSelection
+		.append("rect").attr("fill", function(d) { return "blue"; }), 
+		"item");
+		
+	itemGroupSelection.exit().remove();
+}
 
 /* Board renderer */
 
@@ -47,56 +110,10 @@ Template.board.rendered = function () {
   self.node = self.find("svg");
 	
   if (! self.handle) {
-    self.handle = Meteor.autorun(function () {
-			
-      // Append Boxes to the existing box rects.
-      var boxesSelection = 
-				d3.select(self.node).select(".boxZone").selectAll("rect .box")
-        	.data(Boxes.find().fetch(), function (box) { return box._id; });
-
-			// Sync the Boxes data with the box rects. Add drag behavior to them.
-			boxesSelection.enter().append("rect").call(drag);
-      syncCommonRectAttrs(boxesSelection, "box")
-			.attr("fill", function(d) { return "red"; });
-				
-			// Create <foreignObject> elements for each box to hold sums.
-      var sumLabels = 
-				d3.select(self.node).select(".sumLabels").selectAll("foreignObject")
-					.data(Boxes.find().fetch(), function (box) { return box._id; });
-									
-			sumLabels.enter().append("foreignObject").classed("sumLabel", true)
-				.attr("_id", function (box) { return box._id; })
-				.attr("x", function (box) { return box.x + box.width - 100; })
-				.attr("y", function (box) { return box.y + box.height - 44; })
-				.attr("width", function (box) { return 100; })
-				.attr("height", function (box) { return 44; });
-			// Populate those foreign objects with the sum template.
-			$('.sumLabel').each(function (index, foreignObject) {
-				// Be careful to give the search criteria to find(), not to fetch().
-				var theBox = 
-					Boxes.find({ _id: $(foreignObject).attr("_id") }).fetch()[0];
-				
-				$(foreignObject).append(
-					"<body xmlns=\"http://www.w3.org/1999/xhtml\"></body>")
-				.append(Template.sumContainer(theBox));					
-			});
-			
-			sumLabels.exit().remove();
-				
-      var itemDrawings = 
-				d3.select(self.node).select(".boxZone").selectAll("g .item")
-        	.data(Items.find().fetch(), function (item) { return item._id; });
-					
-			var itemGroupSelection = itemDrawings.enter().append("g").call(groupdrag);
-					
-			// Set up the text.
-			setD3GroupAttrsWithProplist(
-				itemGroupSelection.append("text").text(function (d) { return d.title; }), 
-				["_id", "x", "y", "width", "height"]);
-				
-      syncCommonRectAttrs(itemGroupSelection
-				.append("rect").attr("fill", function(d) { return "blue"; }), 
-				"item");
+    self.handle = Meteor.autorun(function () {			
+			setUpBoxes(self.node);
+			setUpSums(self.node);
+			setUpItems(self.node);				
 		});
 	}	
 };
