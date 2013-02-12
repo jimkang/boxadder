c349beafea41f7d0a2c4ccbcad01d6cc9ac9bb26
Jim Kang <jimkang@gmail.com>
2013-02-12 03:26:47 -0500

    Chained all append calls in setUpItems to the enter() call so that when re-rendering happens, extra rects and texts are not appended to the item g.
    Added a stop handler to the drag behavior. It saves the x and y changes from the dragged element. The problem is that on the second rerender that's triggered as a result of dragging something, the x and y are not set up correctly on the dragged item.

--- client/board.js
+++ client/board.js
@@ -11,22 +11,46 @@ function setD3GroupAttrsWithProplist(group, propnames) {
 };
 	
 // Drag behaviors that can be added to SVG selections.
-function dragmove(d) {
-  d3.select(this)
-    .attr("x", d.x = d3.event.x)
-    .attr("y", d.y = d3.event.y);
-}
-var drag = d3.behavior.drag().origin(Object).on("drag", dragmove);	
 
 function groupdragmove(d) {
+	d.x += d3.event.dx;
+	d.y += d3.event.dy;
+	console.log(d.x + ", " + d.y);
+	
   d3.selectAll(this.childNodes)
     .attr("x", function(item) { 
-			item.x = parseInt($(this).attr("x")) + d3.event.dx; return item.x; } )
+			return parseInt($(this).attr("x")) + d3.event.dx;  } )
     .attr("y", function(item) { 
-			item.y = parseInt($(this).attr("y")) + d3.event.dy; return item.y; });
+			return parseInt($(this).attr("y")) + d3.event.dy; });
 }
 
-var groupdrag = d3.behavior.drag().origin(Object).on("drag", groupdragmove);	
+function saveAndRecalcOnDragStop(d) {
+	// First, commit the changes to what was dragged.
+	
+	// TODO: Might be a neater way to do this.	
+	var collection = null;
+	var className = $(this).attr('class');
+	if (className == 'item')
+	{
+		collection = Items;
+	}
+	else if (className == 'box')
+	{
+		collection = Boxes;
+	}
+	console.log("Saving " + d._id + " to: " + d.x + ", " + d.y);
+	
+	collection.update(d._id, {$set: {x: d.x, y: d.y}});
+	
+	// Then, recalculate the sums.
+	// // Recalculate the sums.
+	// d3.selectAll(".sum").text(function (box) { 
+	// 	console.log("hi!"); return sumForBox(box); });
+// });
+}
+
+var groupdrag = d3.behavior.drag().origin(Object).on("drag", groupdragmove)
+.on("dragend", saveAndRecalcOnDragStop);
 
 function syncCommonRectAttrs(group, cssClass) {
 	return setD3GroupAttrsWithProplist(group, 
@@ -51,24 +75,23 @@ function setUpBoxes(svgNode) {
     	.data(Boxes.find().fetch(), function (box) { return box._id; });
 
   // Sync the <g> elements to the box records. Add the drag behavior.
-	boxGroupsSelection.enter().append("g").call(groupdrag);
+	boxGroupsSelection.enter().append("g").classed("box", true).call(groupdrag);
 
 	// Set up the rect and its position and color. Append it first so that it is 
 	// the furthest back, z-order-wise.
   syncCommonRectAttrs(boxGroupsSelection.append("rect")
 		.attr("fill", function(d) { return "white"; })
-		.attr("stroke", function (d) { return "red"; }),
-		"box");
+		.attr("stroke", function (d) { return "red"; }));
 		
 	// Add the sum box and label to the boxes.
-	boxGroupsSelection.append("rect").classed("sum", true)
+	boxGroupsSelection.append("rect")
 	.attr("stroke", "red").attr("fill", "orange")
 	.attr("x", function (box) { return box.x + box.width - 100; })
 	.attr("y", function (box) { return box.y + box.height - 44; })
 	.attr("width", function (box) { return 100; })
 	.attr("height", function (box) { return 44; });
 	
-	boxGroupsSelection.append("text").text(
+	boxGroupsSelection.append("text").classed("sum", true).text(
 		function (box) { return sumForBox(box); })
 		.attr("x", function (box) { return box.x + box.width - 100/2; })
 		.attr("y", function (box) { return box.y + box.height - 44/2 + 4; })
@@ -80,35 +103,47 @@ function setUpBoxes(svgNode) {
 }
 
 function setUpItems(svgNode) {
+	
+	var items = Items.find().fetch();
+	var itemIdFunction = function (item) { return item._id; };
+	var boxZoneSelection = d3.select(svgNode).select(".boxZone");
+	
 	// Sync the <g>s and the data.
   var itemGroupSelection = 
-		d3.select(svgNode).select(".boxZone").selectAll("g .item")
-    	.data(Items.find().fetch(), function (item) { return item._id; });
+		boxZoneSelection.selectAll("g .item").data(items, itemIdFunction);
 					
-	itemGroupSelection.enter().append("g").call(groupdrag);
+	itemGroupSelection.enter().append("g").classed("item", true).call(groupdrag)
+	// Append the rect first so that it is the furthest back, z-order-wise.
+	.call(function (groupElement) { groupElement.append("rect"); })
+	.call(function (groupElement) { 
+		groupElement.append("text").classed("itemtitle", true); })
+	.call(function (groupElement) { 
+		groupElement.append("text").classed("score", true); });
 	
-	// Set up the rect and its position and color. Do this first so that it is 
-	// the furthest back, z-order-wise.
-  syncCommonRectAttrs(itemGroupSelection
-		.append("rect").attr("fill", function(d) { return "blue"; }), 
-		"item");
-					
+	itemGroupSelection.exit().remove();
+		
+	// Set up the rect and its position and color.	
+  syncCommonRectAttrs(itemGroupSelection.selectAll("rect"))
+	.attr("fill", function(d) { return "blue"; });
+		
 	// Set up the title label.
 	setD3GroupAttrsWithProplist(
-		itemGroupSelection.append("text").text(function (d) { return d.title; }), 
+		// d3 selectors are slightly different from jQuery's: No spaces for 
+		// multiple specifiers that are to be ANDed.
+		itemGroupSelection.selectAll("text.itemtitle")
+		.text(function (d) { return d.title; }), 
 		["x", "width", "height"])
 		.attr("y", function (item) { return item.y + 44/2; })
 		.attr("fill", function (item) { return "white"; });
 		
 	// Set up the score field.
-	itemGroupSelection.append("text").text(function (d) { return d.score; })
+	itemGroupSelection.selectAll("text.score")
+		.text(function (d) { return d.score; })
 		.attr("x", function (item) { return 100 + item.x; })
 		.attr("y", function (item) { return item.y + 44/2; })
 		.attr("width", function (item) { return 100; })
 		.attr("height", function (item) { return 44; })
-		.attr("fill", function (item) { return "white"; });
-			
-	itemGroupSelection.exit().remove();
+		.attr("fill", function (item) { return "white"; });			
 }
 
 /* Board renderer */
