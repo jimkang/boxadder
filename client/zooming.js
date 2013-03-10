bd758f31559dd0b963435de7174e78d4c45d35b5
Jim Kang <jimkang@gmail.com>
2013-03-09 19:59:58 -0500

    - After lockZoomToDefaultCenterPanAtDataCoords, restore (during unlockZoom) to whatever zoom and pan level the user was at before that locking. This will prevent the unexpected behavior in which a user edits while zoomed in, is zoomed out to 1 to edit, the user completes the edit, then next time they zoom, they are snapped back the zoom level that they were at before they started editing. - Pulled zoom out of setUpZoomOnBoard.

--- client/zooming.js
+++ client/zooming.js
@@ -1,6 +1,7 @@
 
 var BoardZoomer = {
 	locked: false,
+	boxZoneSelection: null,	
 	setUpZoomOnBoard: function() {		
 		// Make x and y scaling functions that just returns whatever is passed into 
 		// them (same domain and range).
@@ -17,22 +18,22 @@ var BoardZoomer = {
 		// When zoom and pan gestures happen inside of #boardSVG, have it call the 
 		// zoom function to make changes.
 		d3.select("#boardSVG").call(
-			d3.behavior.zoom().x(x).y(y).scaleExtent([0.0625, 4]).on("zoom", zoom)
+			d3.behavior.zoom().x(x).y(y).scaleExtent([0.0625, 4])
+			.on("zoom", BoardZoomer.zoom)
 		);
 	
-		var boxZoneSelection = d3.select("g.boxZone");
-
-		// This function applies the zoom changes to the <g> element rather than
-		// the <svg> element because <svg>s do not have a transform attribute. 
-		// The behavior is connected to the <svg> rather than the <g> because then
-		// dragging-to-pan doesn't work otherwise. Maybe something cannot be 
-		// transformed while it is receiving drag events?
-		function zoom() {
-			if (!BoardZoomer.locked) {
-				boxZoneSelection.attr("transform", 
-					"translate(" + d3.event.translate + ")" + 
-					" scale(" + d3.event.scale + ")");
-			}
+		BoardZoomer.boxZoneSelection = d3.select("g.boxZone");
+	},
+	// This function applies the zoom changes to the <g> element rather than
+	// the <svg> element because <svg>s do not have a transform attribute. 
+	// The behavior is connected to the <svg> rather than the <g> because then
+	// dragging-to-pan doesn't work otherwise. Maybe something cannot be 
+	// transformed while it is receiving drag events?
+	zoom: function() {
+		if (!BoardZoomer.locked) {
+			BoardZoomer.boxZoneSelection.attr("transform", 
+				"translate(" + d3.event.translate + ")" + 
+				" scale(" + d3.event.scale + ")");
 		}
 	},
 	resetZoom: function() {	
@@ -49,21 +50,33 @@ var BoardZoomer = {
 		console.log("locked!");
 		BoardZoomer.locked = true;
 	},
+	preLockZoomTransformString: null,
 	unlockZoom: function() {
 		console.log("unlocked!");
 		BoardZoomer.locked = false;
+		// If preLockZoomTransformString is set, restore the zoom transform to that.
+		if (BoardZoomer.preLockZoomTransformString) {
+			BoardZoomer.boxZoneSelection.attr('transform', 
+				BoardZoomer.preLockZoomTransformString);
+			BoardZoomer.preLockZoomTransformString = null;
+		}
 	},
 	lockZoomToDefaultCenterPanAtDataCoords: function(d) {
 		var boxZoneSel = $('.boxZone');
 		var boardSel = $('#boardSVG');
 		var boardWidth = parseInt(boardSel.attr('width'));
 		var boardHeight = parseInt(boardSel.attr('height'));
+		
+		// unlockZoom will restore the zoom transform to this.
+		BoardZoomer.preLockZoomTransformString = 
+			BoardZoomer.boxZoneSelection.attr('transform');
+		
 		var newTransformString = 'translate(' + 
 			(-d.x + boardWidth/2) + ', ' + (-d.y + boardHeight/2) + 
 			') scale(1)';		
 		// console.log("Setting transform to: ", newTransformString);
 		boxZoneSel.attr('transform', newTransformString);
 		d3.event.scale = 1.0;
-		BoardZoomer.locked = true;
+		BoardZoomer.lockZoom();
 	}
 }
